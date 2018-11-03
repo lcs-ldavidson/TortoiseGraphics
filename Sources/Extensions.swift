fea3649e53d53d2a5fc4c3a2d6874f72269541fb
Liam Davidson <ldavidson@lcs.on.ca>
2018-11-03 10:49:48 -0400

    Now I just have to add a bunch of flourishes.

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -9,20 +9,20 @@ import Foundation
 import CoreGraphics
 
 public extension Tortoise {
-    
+
     func square(withSize size: Double) {
-        
+
         self.penDown()
         for _ in 1...4 {
             self.hideTortoise()
-            
+
             self.penUp()
-            
+
             self.forward(size/2)
             self.left(90)
             self.forward(size/2)
             self.left(180)
-            
+
             self.penDown()
             for _ in 1...4 {
                 self.forward(size)
@@ -33,46 +33,46 @@ public extension Tortoise {
             self.right(90)
             self.forward(size/2)
             self.right(180)
-            
+
             self.penDown()
         }
     }
-    
+
     func randomShape(withSize size: Double) {
-        
+
         self.penDown()
-        
+
         var distance = size * random(5)
-        
+
         self.forward(distance)
         self.right(180)
         self.right(random(90))
         self.left(180)
         self.left(random(90))
-        
+
     }
-    
+
     func curve(withSize size: Double, drawSides sides: Int, maxPenSize penSize: Int) {
-        
+
         var brushSize = 15
         var brushUpOrDown = 0
-        
+
         self.penDown()
-        
+
         for sidesDrawn in 1...Int(sides) {
-            
+
             self.penSize(Double(brushSize))
-            
+
             self.forward(size)
-            
+
             if size >= 0 {
                 self.right(1.8)
             } else {
                 self.left(1.8)
             }
-            
+
             //changing whether or not the brush is changing
-            
+
             if sidesDrawn < (sides/3) {
                 brushUpOrDown = 0
             } else if sidesDrawn >= (sides/3) && sidesDrawn < ((sides/3) * 2) {
@@ -80,21 +80,23 @@ public extension Tortoise {
             } else if sidesDrawn > ((sides/3) * 2) {
                 brushUpOrDown = 2
             }
-            
+
             //changing the brush size
-            
+
             if brushUpOrDown == 0 {
-                
-                brushSize += 2
-                
+                if brushSize <= penSize {
+                    brushSize += 2
+                }
             } else if brushUpOrDown == 1 {
-                
+
             } else if brushUpOrDown == 2 {
+                if penSize >= 5 {
                 brushSize -= 2
+                }
             }
-            
+
         }
-        
+
     }
-    
+
 }
