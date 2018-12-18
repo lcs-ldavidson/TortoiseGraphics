b42e3c1418b99d913d842d9fd0f6bf5a246f3c8d
Liam Davidson <ldavidson@lcs.on.ca>
2018-12-18 10:11:36 -0500

    Good progress. Working on making pincers and a head that scales with the body.

--- LineArt/CanvasView.swift
+++ LineArt/CanvasView.swift
@@ -18,7 +18,7 @@ class CanvasView: NSView {
             
             //set turtle qualities
             turtle.penColor(Color.black)
-            turtle.penSize(3)
+            turtle.penSize(1)
             turtle.goto(0, 0)
             turtle.hideTortoise()
             turtle.setH(turtle.random(360))
@@ -31,7 +31,6 @@ class CanvasView: NSView {
                 
                 //draw the set of legs
                 turtle.penUp()
-                
                 turtle.drawLegs(atX: turtle.ycor, atY: turtle.ycor, legLength: length)
                 
                 //increase the next legs' length
@@ -44,6 +43,13 @@ class CanvasView: NSView {
                 
             }
             
+            //tells me what length currently is
+            print(length)
+            
+            var centerHeadX = turtle.xcor
+            var centerHeadY = turtle.ycor
+            var centerHeading = turtle.heading
+            
             //drawing head
             turtle.left(90)
             turtle.forward(length)
@@ -54,21 +60,74 @@ class CanvasView: NSView {
             turtle.endFill()
             
             //drawing left eye
+            turtle.right(90)
+            turtle.forward(5)
+            turtle.left(90)
             turtle.penColor(Color.white)
             turtle.fillColor(Color.white)
             turtle.beginFill()
-            turtle.drawcurve(sideLength: 3, drawSides: Int(length), withtotalSides: Int(length), curveRight: true)
+            turtle.drawcurve(sideLength: 2, drawSides: Int(length), withtotalSides: Int(length), curveRight: true)
+            turtle.endFill()
+            
+            //drawing left pupil
+            turtle.fillColor(Color.black)
+            turtle.beginFill()
+            turtle.drawcurve(sideLength: 2, drawSides: 30, withtotalSides: 30, curveRight: true)
+            turtle.endFill()
+            
+            //drawing left shine
+            turtle.fillColor(Color.white)
+            turtle.beginFill()
+            turtle.drawcurve(sideLength: 1, drawSides: 20, withtotalSides: 20, curveRight: true)
             turtle.endFill()
             
             //drawing right eye
+            turtle.fillColor(Color.white)
             turtle.penUp()
             turtle.right(90)
-            turtle.forward(length * 2)
+            turtle.forward(length * 2 - 10)
             turtle.left(90)
             turtle.penDown()
             turtle.beginFill()
-            turtle.drawcurve(sideLength: 3, drawSides: Int(length), withtotalSides: Int(length), curveRight: false)
+            turtle.drawcurve(sideLength: 2, drawSides: Int(length), withtotalSides: Int(length), curveRight: false)
             turtle.endFill()
+            
+            //drawing right pupil
+            turtle.fillColor(Color.black)
+            turtle.beginFill()
+            turtle.drawcurve(sideLength: 2, drawSides: 30, withtotalSides: 30, curveRight: false)
+            turtle.endFill()
+            
+            //drawing right pupil
+            turtle.fillColor(Color.white)
+            turtle.penUp()
+            turtle.left(90)
+            turtle.forward(10)
+            turtle.penDown()
+            turtle.beginFill()
+            turtle.drawcurve(sideLength: 1, drawSides: 20, withtotalSides: 20, curveRight: false)
+            turtle.endFill()
+            
+            
+            //draw pincers
+            turtle.penColor(Color.black)
+            turtle.fillColor(Color.black)
+            turtle.penUp()
+            
+            turtle.right(90)
+            if length > 39 {
+            turtle.forward(30)
+            } else {
+                turtle.forward(20)
+            }
+            
+            turtle.penDown()
+            turtle.beginFill()
+            turtle.drawcurve(sideLength: 1, drawSides: 35, withtotalSides: 360, curveRight: false)
+            turtle.left(150)
+            turtle.drawcurve(sideLength: 1, drawSides: 35, withtotalSides: 360, curveRight: true)
+            turtle.endFill()
+            
             
             
             
